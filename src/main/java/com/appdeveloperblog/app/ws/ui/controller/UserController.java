a90bc0d759d884b3d91ae803a1d300b2e1fe229b
Upendra Singh <letsmailupendra@gmail.com>
2024-04-25 08:45:31 +0530

    Handle HTTP GET request

--- src/main/java/com/appdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,12 +3,20 @@ package com.appdeveloperblog.app.ws.ui.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users") // http://localhost:8080/users
+@RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
     @GetMapping
-    public String getUser() {
-        return "Get User Called";
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "30") int limit,
+                            @RequestParam(value="sort", defaultValue = "desc", required = false) String sort
+    ) {
+        return "Get Users called for page = " + page + " and number of records = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable("userId") String userId) {
+        return "Get User Called for userId = " + userId;
     }
 
     @PostMapping
