ace6ef1c25afb55eed79c58274865e626534d2f6
Upendra Singh <letsmailupendra@gmail.com>
2024-04-25 21:22:59 +0530

    Update UserController to return data in multiformats

--- src/main/java/com/appdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,36 +1,45 @@
 package com.appdeveloperblog.app.ws.ui.controller;
 
+import com.appdeveloperblog.app.ws.ui.model.response.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users") // http://localhost:8080/users
+@RequestMapping("/users")
 public class UserController {
 
     @GetMapping
-    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
-                           @RequestParam(value = "limit", defaultValue = "30") int limit,
-                            @RequestParam(value="sort", defaultValue = "desc", required = false) String sort
-    ) {
-        return "Get Users called for page = " + page + " and number of records = " + limit + " and sort = " + sort;
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "30") int limit, @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        String response = "Get Users called for page = " + page + " and number of records = " + limit + " and sort = " + sort;
+
+        return response;
     }
 
-    @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable("userId") String userId) {
-        return "Get User Called for userId = " + userId;
+    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest getUser(@PathVariable("userId") String userId) {
+        UserRest userDetails = new UserRest();
+        userDetails.setFirstName("Upendra");
+        userDetails.setLastName("Singh");
+        userDetails.setEmail("temp@mail.com");
+
+        return userDetails;
     }
 
     @PostMapping
     public String createUser() {
-        return "Create User Called";
+        String createResponse = "Create User Called";
+        return createResponse;
     }
 
     @PutMapping
     public String updateUser() {
-        return "Update User Called";
+        String updateResponse = "Update User Called";
+        return updateResponse;
     }
 
     @DeleteMapping
     public String deleteUser() {
-        return "Delete User Called";
+        String deleteResponse = "Delete User Called";
+        return deleteResponse;
     }
 }
